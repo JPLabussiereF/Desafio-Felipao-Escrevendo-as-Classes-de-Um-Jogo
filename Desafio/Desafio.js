2d9256a8f099b3e8855939f8fa53cc697324505c
JPLabussiereF <labussework@gmail.com>
2024-02-24 21:19:08 -0300

    Sucesso no teste 1

--- Desafio/Desafio.js
+++ Desafio/Desafio.js
@@ -1,18 +1,45 @@
 class heroiTipo{
-    constructor(name, age, type, attack){
+    constructor(name, age, type){
 
         this.name = name;
         this.age = age;
         this.type = type;
-        this.attack = attack;
     }
 
     toAttack(){
-        console.log(`O ${this.type}  atacou usando ${this.attack}`)
+
+        let attack;
+
+        switch(this.type){
+
+            case "Assassino":
+                attack = "sua lâmina"
+            break; 
+
+        }
+        
+        console.log(`O ${this.type} atacou usando ${attack}!`)
 
     }
     
 }
+
+const champion1 = new heroiTipo("Talon", 30, "Assassino");
+
+champion1.toAttack()
+
+
+
+
+// Assassino
+// Mago;
+// Lutador;
+// Atirador;
+// Controlador ou Suporte;
+// Tanque;
+
+// colocar tipo - mago // etc
+// colocar ataque - tipo do ataque // etc
 
 // além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
 
